b99fb5b3bab6c8b13ec7ed361b8cce438a2c354a
Lee-j-s <ashfejd12@naver.com>
2024-04-30 12:27:28 +0900

    커밋 메시지

--- 3.py
+++ 3.py
@@ -1,40 +1,29 @@
-from flask import Flask, jsonify
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-app = Flask(__name__)
+# Firefox를 사용하기 위한 WebDriver 설정
+driver = webdriver.Firefox()
 
-@app.route('/scrape')
-def scrape_data():
-    # Firefox를 사용하기 위한 WebDriver 설정
-    driver = webdriver.Firefox()
+# HTML 코드
+html_content = '''
+<div class="swiper-slide swiper-slide-next" data-swiper-slide-index="4" style="margin-right: 10px;">
+    <a href="https://cu.bgfretail.com/brand_info/news_view.do?category=brand_info&amp;depth2=5&amp;idx=1024" title="VIP 고객님을 위한 4월 특별 혜택관" target=""> <!-- 이미지 링크 변경 필요 -->
+        <picture>
+        <source media="(max-width: 767px)" srcset="https://cdn2.bgfretail.com/bgfbrand/files/newmainImage/74CCA257408B4B1CA3FCB0E5EDD9928C.jpg">
+        <source media="(min-width: 768px)" srcset="https://cdn2.bgfretail.com/bgfbrand/files/newmainImage/0A9C46355A514FDD80E805BB3C96EBF4.jpg">
+        <img src="https://cdn2.bgfretail.com/bgfbrand/files/newmainImage/0A9C46355A514FDD80E805BB3C96EBF4.jpg" alt="관리자 등록 이미지"> </picture>
+    </a>
+</div>
+'''
 
-    # HTML 코드
-    html_content = '''
-    <div class="swiper-slide swiper-slide-next" data-swiper-slide-index="4" style="margin-right: 10px;">
-        <a href="https://cu.bgfretail.com/brand_info/news_view.do?category=brand_info&amp;depth2=5&amp;idx=1024" title="VIP 고객님을 위한 4월 특별 혜택관" target=""> <!-- 이미지 링크 변경 필요 -->
-            <picture>
-            <source media="(max-width: 767px)" srcset="https://cdn2.bgfretail.com/bgfbrand/files/newmainImage/74CCA257408B4B1CA3FCB0E5EDD9928C.jpg">
-            <source media="(min-width: 768px)" srcset="https://cdn2.bgfretail.com/bgfbrand/files/newmainImage/0A9C46355A514FDD80E805BB3C96EBF4.jpg">
-            <img src="https://cdn2.bgfretail.com/bgfbrand/files/newmainImage/0A9C46355A514FDD80E805BB3C96EBF4.jpg" alt="관리자 등록 이미지"> </picture>
-        </a>
-    </div>
-    '''
+# HTML 코드를 로드하여 파싱
+driver.get("data:text/html;charset=utf-8,{html_content}".format(html_content=html_content))
 
-    # HTML 코드를 로드하여 파싱
-    driver.get("data:text/html;charset=utf-8,{html_content}".format(html_content=html_content))
+# 요소 찾기
+element = driver.find_element(By.CLASS_NAME, 'swiper-slide')
 
-    # 요소 찾기
-    element = driver.find_element(By.CLASS_NAME, 'swiper-slide')
+# 결과 출력
+print(element.get_attribute('outerHTML'))
 
-    # 결과 출력
-    outerHTML = element.get_attribute('outerHTML')
-
-    # 브라우저 종료
-    driver.quit()
-
-    # JSON 형식으로 결과 반환
-    return jsonify({'result': outerHTML})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# 브라우저 종료
+driver.quit()
--- 5.py
+++ 5.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-def get_image_url():
+def get_image_urls():
     # Chrome WebDriver 초기화
     driver = webdriver.Chrome()
     
@@ -18,21 +18,23 @@ def get_image_url():
     soup = BeautifulSoup(html_content, "html.parser")
     
     # 이미지 URL 찾기
-    img_element = soup.find("img")
-    img_url = img_element["src"]
+    img_urls = []
+    img_elements = soup.find_all("img")
+    for img_element in img_elements:
+        img_urls.append(img_element["src"])
     
     # WebDriver 종료
     driver.quit()
     
-    return img_url
+    return img_urls
 
 @app.route('/scrape')
 def scrape_data():
     # 이미지 URL 가져오기
-    img_url = get_image_url()
+    img_urls = get_image_urls()
     
     # JSON 형식으로 결과 반환
-    return jsonify({'image_url': img_url})
+    return jsonify({'image_urls': img_urls})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9000)
+    app.run(debug=True, port=4000)
